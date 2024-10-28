3a4bf8562616dad89f42211c4aa902f7ec8853b2
Saiavala18 <23a91a1269@aec.edu.in>
2024-10-29 00:15:34 +0530

    Uploading code: Profit or Loss.c

---
+++ Profit or Loss.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    printf("Loss");
+    else if(b>a)
+    printf("Profit");
+    else
+    printf("No Profit and No Loss");
+}
