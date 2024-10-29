3f4b6724254e1586026ffba72e103efec6140ea8
Saiavala18 <23a91a1269@aec.edu.in>
2024-10-29 22:38:51 +0530

    Uploading code: Spy Number.c

---
+++ Spy Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n,s=0,p=1;
+    scanf("%d",&n);
+    while(n>0){
+        int r=n%10;
+        s+=r;
+        p*=r;
+        n=n/10;
+    }
+    if(s==p)
+    printf("Spy Number");
+    else
+    printf("Not Spy Number");
+    
+}
