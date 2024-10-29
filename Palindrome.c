5ee7cfadd9103732d1937e192e682849189e37b4
Saiavala18 <23a91a1269@aec.edu.in>
2024-10-29 21:55:45 +0530

    Uploading code: Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,rev=0;
+    scanf("%d",&n);
+    int temp=n;
+    while(n>0){
+        int r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    if(temp==rev)
+    printf("Palindrome");
+    else
+    printf("Not Palindrome");
+}
