1a982f516d6b55bcb238f92c691af3190d30a6a9
SAI <23a91a1269@aec.edu.in>
2024-11-09 15:20:10 +0530

    Uploading code: Sum of Even Positions.c

---
+++ Sum of Even Positions.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,s=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        if(i%2==0)
+        s+=a[i];
+    }
+    printf("%d",s);
+
+}
