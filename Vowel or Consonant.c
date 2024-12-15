54a0a7787e76058340ebb22e32be51e7e539d29d
SAI <23a91a1269@aec.edu.in>
2024-12-15 20:32:49 +0530

    Uploading code: Vowel or Consonant.c

--- Vowel or Consonant.c
+++ Vowel or Consonant.c
@@ -2,8 +2,9 @@
 int main(){
     char c;
     scanf("%c",&c);
-    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
-    printf("VOWEL");
-    else
-    printf("CONSONANT");
-}
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+        printf("VOWEL");
+    }else{
+        printf("CONSONANT");
+    }
+    }
